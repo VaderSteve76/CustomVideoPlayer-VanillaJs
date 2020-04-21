d8eb9e502035cb04ac8b917c57d8e92845597e1d
VaderSteve76 <alverson.s@yahoo.com>
2020-04-21 18:11:41 -0400

    Added functions for play& pause video as well as play&pause icon

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
+// Play & pause video
+function toggleVideoStatus() {
+  if(video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+// Update play &pause icon
+function updatePlayIcon() {
+  if(video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x></i>';
+  }
+};
+
 // Event Listeners
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('play', updatePlayIcon);
